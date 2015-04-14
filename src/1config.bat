ca864adb0f7c432804f8a91d04ea98f0b8a45b05
nanoko <yy845jp@gmail.com>
2015-04-14 20:53:27 +0900

    for 1.6(2)

--- src/1config.bat
+++ src/1config.bat
@@ -1,7 +1,7 @@
 rem Å´à»â∫ÇÇ©Ç¶ÇƒÇ›ÇÈ
 SET TIMIDITYHOME=e:\Timidity++
 SET CSAKURAHOME=c:\Users\Ç‰Ç¡ÇΩ\Dropbox\NeetSoundProgram\csakura
-SET PERLHOME=c:\perl
+SET PERLHOME=c:\perl64
 rem SET TIMIDITYHOME=c:\t\Timidity++
 SET INPUT=e:\bgm\isischan
 SET OUTPUT=%input%
